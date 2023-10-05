a05a387ed6d9567254c0e03602558fae5d0f9f59
Ghilai Levin <ghilailevin97@gmail.com>
2023-10-04 21:07:06 -0300

    NodeJS

--- src/Index.js
+++ src/Index.js
@@ -8,7 +8,10 @@ const __dirname = dirname(fileURLToPath(import.meta.url))
 
 app.set("views", join(__dirname, "views"))
 app.set("view engine", "ejs")
+
 app.use(IndexRoutes)
+
+app.use(express.static(join(__dirname, "public")))
 
 app.listen(3000)
 console.log("server is listening on port", 3000)
